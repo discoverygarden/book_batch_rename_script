9081cdc724a38328775291381ec9c9b74f1cc5d6
daitken <daitken@discoverygarden.ca>
2013-03-08 08:33:22 -0400

    tiffs without dngs, copy of original
    Now able to do tiffs without dngs, tiffs with some dngs, or tiffs with dngs for every one.
    
    Also now leaves a copy of the original file in case sorting went south.

--- rename_script.bat
+++ rename_script.bat
@@ -7,31 +7,41 @@ echo * This batch script takes a folder full of book files and sorts them into  
 echo * a folder that can be uploaded to the server staging area to be ingested.  *
 echo * It is compatible with .tif files that are paired with .dng negatives. It  *
 echo * accepts the first .mrc and .xml metadata file it finds, and adds in any   *
-echo * files containing the word 'colourchecker'. Only put in one .xml or .mrc.  *
+echo * files containing the word 'colourchecker'. Only put in one .xml or .mrc;  *
+echo * duplicates will be overwritten.                                           *
 echo *                                                                           *
-echo * WARNING: CHECK THE DOCUMENTATION FOR INSTRUCTIONS ON HOW TO SORT THE BOOK *
-echo * FILES IN PREPARATION FOR USING THIS SCRIPT! MAKE A BACKUP OF THE FILES!   *
-echo * INCORRECTLY NAMED FILES WILL CAUSE SEVERE SORTING ERRORS THAT WILL BE     *
-echo * EXTREMELY DIFFICULT TO RECOVER FROM! IF YOU ARE NOT SURE THAT YOUR FILES  *
-echo * ARE NAMED CORRECTLY, EXIT THIS SCRIPT NOW AND CHECK THE DOCUMENTATION!    *
+echo * This script will ask you for the name of the book and make a copy of all  *
+echo * your files into a folder with the name of the book you gave. It will also *
+echo * keep the originals in the folder they were in. Please make sure there is  *
+echo * enough space for the script to make duplicates of all the book files.     *
+echo *                                                                           *
+echo * PLEASE CHECK THE DOCUMENTATION FOR INSTRUCTIONS ON HOW TO NAME YOUR FILES *
+echo * BEFORE RUNNING THIS SCRIPT. The script cannot check for mistakes in the   *
+echo * original filenames and will sort indiscriminately. If you notice files    *
+echo * sorted incorrectly in the new directory, remove it, check your file names *
+echo * and try again. Otherwise, you may remove the originals if you wish.       *
 echo ***************************************************************************** 
 pause
 :: Ask for the book name
 set /p book="Name of book: "
 mkdir "%book%"
+mkdir "%book%"_tmp
 :: Throw in the first XML found
 for /f "tokens=1" %%x in ('dir /b *.xml') do (
-	rename "%%x" "--METADATA--.xml"
+	copy "%%x" "--METADATA--.xml"
 	move "--METADATA--.xml" "!book!"
+	move "%%x" "!book!"_tmp
 	)
 :: Move over all colourchecker files
 for /f %%c in ('dir /b *colourchecker*') do (
-	move *colourchecker* "!book!"
+	copy *colourchecker* "!book!"
+	move *colourchecker* "!book!"_tmp
 	)
 :: Throw in first MARC binary found
 for /f %%m in ('dir /b *.mrc') do (
-	rename "%%m" "--METADATA--.mrc"
+	copy "%%m" "--METADATA--.mrc"
 	move "--METADATA--.mrc" "!book!"
+	move "%%m" "!book!"_tmp
 	)
 :: Set folder count
 set /a c=1
@@ -44,18 +54,23 @@ for /f "tokens=*" %%t in ('dir /b *.tif*') do (
 	for /f "tokens=*" %%d in ('dir /b *.dng') do (
 		set filenamed=%%~nd
 		if "!filenamet!"=="!filenamed!" (
-			rename "%%t" OBJ.tif
+			copy "%%t" OBJ.tif
 			move OBJ.tif "!c!"
-			rename "%%d" DNG.dng
+			copy "%%d" DNG.dng
 			move DNG.dng "!c!"
 			move "!c!" "!book!"
+			move "%%t" "!book!"_tmp
+			move "%%d" "!book!"_tmp
 			set /a c=c+1
 			goto tiffdngloop
 			)
 		)
-	rename "%%t" OBJ.tif
+	copy "%%t" OBJ.tif
 	move OBJ.tif "!c!"
 	move "!c!" "!book!"
+	move "%%t" "!book!"_tmp
 	set /a c=c+1
 	)
+move "!book!"_tmp\*.* .\
+rmdir "!book!"_tmp
 ENDLOCAL
