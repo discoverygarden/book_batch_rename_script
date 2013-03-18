5093e61ee87118a6be1e739c9c93a3c8a78ad4aa
daitken <daitken@discoverygarden.ca>
2013-03-18 19:35:40 -0300

    edited windows batch for standatd 7.x

--- rename_script.bat
+++ rename_script.bat
@@ -4,10 +4,10 @@ echo ***************************************************************************
 echo *                    ISLANDORA BOOK BATCH RENAME SCRIPT                     *
 echo *                                                                           *
 echo * This batch script takes a folder full of book files and sorts them into   *
-echo * a folder that can be uploaded to the server staging area to be ingested.  *
-echo * It is compatible with .tif files that are paired with .dng negatives. It  *
-echo * accepts the first .mrc and .xml metadata file it finds, and adds in any   *
-echo * files containing the word 'colourchecker'. Only put in one .xml or .mrc.  *
+echo * a folder that can be added to a .zip file compatible with the Book Batch  *
+echo * module. It accepts the first .mrc and .xml metadata file it finds, and    *
+echo * all .tif or .tiff files, in alphabetical order. Only put in one .xml or   *
+echo * .mrc.                                                                     *
 echo *                                                                           *
 echo * This script will ask you for the name of the book and make a copy of all  *
 echo * your files into a folder with the name of the book you gave. It will also *
@@ -37,33 +37,13 @@ if exist *.mrc (
 	move --METADATA--.mrc "!book!"
 	move "%%m" "!book!"_tmp
 	)
-:: Move over all colourchecker files
-if exist *colourchecker* (
-	copy *colourchecker* "!book!"
-	move *colourchecker* "!book!"_tmp
-	)
 :: Set folder count
 set /a c=1
-:: Move over .tiffs and .dngs
-:tiffdngloop
+:: Move over .tiffs
+:tiffloop
 for /f "tokens=*" %%t in ('dir /b *.tif*') do (
 	mkdir !c!
 	set filenamet=%%~nt
-	:: Searches for a matching filename and if found, renames and exits loop
-	for /f "tokens=*" %%d in ('dir /b *.dng') do (
-		set filenamed=%%~nd
-		if "!filenamet!"=="!filenamed!" (
-			copy "%%t" OBJ.tif
-			move OBJ.tif "!c!"
-			copy "%%d" DNG.dng
-			move DNG.dng "!c!"
-			move "!c!" "!book!"
-			move "%%t" "!book!"_tmp
-			move "%%d" "!book!"_tmp
-			set /a c=c+1
-			goto tiffdngloop
-			)
-		)
 	copy "%%t" OBJ.tif
 	move OBJ.tif "!c!"
 	move "!c!" "!book!"
